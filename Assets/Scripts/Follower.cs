fc1068bbb17b91e06de9eda8d9202372b5581d6d
Lennard Backsen <lbacksen@yahoo.de>
2020-12-07 15:35:38 +0100

    Movement Setup

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTravelled;
+
+    private void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+    }
+}
